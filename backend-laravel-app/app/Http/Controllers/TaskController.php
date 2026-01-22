468c0d6a5a4ddcae6581b675ac3f85b71049be10
bright123-max <brightathuman65@gmail.com>
2026-01-22 19:27:59 +0300

    Refactor TaskController index method to retrieve tasks directly from the Task model

--- backend-laravel-app/app/Http/Controllers/TaskController.php
+++ backend-laravel-app/app/Http/Controllers/TaskController.php
@@ -11,9 +11,7 @@ class TaskController extends Controller
 {
     public function index(Request $request)
     {
-        $tasks = $request->user()
-            ->tasks()
-            ->with('category')
+        $tasks = Task::with('category')
             ->latest()
             ->paginate(20);
 
